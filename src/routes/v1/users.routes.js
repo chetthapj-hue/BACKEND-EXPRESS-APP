80806f7086cac39217ccca0081f343abcb31a6e0
Chettha.pj <chettha.pj@gmail.com>
2025-12-29 14:48:01 +0700

    3

---
+++ src/routes/v1/users.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createUser, deleteUser, getUsers, testAPI } from "../../modules/users/users.controller.js";
+
+export const router = Router()
+
+
+router.get("/test", testAPI);
+
+router.get("/", getUsers);
+
+router.post("/", createUser);
+
+// the funtion inside is called Route  Handler /Controller
+router.delete("/:id", deleteUser);
